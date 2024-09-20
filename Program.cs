d11f9b740f23744af3f317b22686809f1290cf20
Muniox <muniox20@gmail.com>
2024-09-20 19:08:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace C__HTTP
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                await httpClient.GetAsync("http://jsonplaceholder.typicode.com/posts");
+            }
+
+
+        }
+    }
+}
